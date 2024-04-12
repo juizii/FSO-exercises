5ee95b094640e8409e4820ba3c19fc2cef65df7b
Joyce Zhang <jzzhang.dev@gmail.com>
2024-04-12 14:13:45 -0400

    Added notes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,25 +2,25 @@ import Header from "./Header"; //Remember to import as well!
 import Content from "./Content";
 import Total from "./Total";
 
-
 const App = () => {
   const course = "Half Stack application development";
-  
-  // Array of parts with their corresponding exercises
+
+  //Array with key values
   const parts = [
     { part: "Fundamentals of React", exercises: 10 },
     { part: "Using props to pass data", exercises: 7 },
     { part: "State of a component", exercises: 14 },
+    //can add other parts here if needed.
   ];
 
   return (
-    <div>
+    <>
       <Header course={course} />
       {parts.map((part, index) => (
         <Content key={index} part={part.part} exercise={part.exercises} />
       ))}
-     <Total parts= {parts}/>
-    </div>
+      <Total parts={parts} />
+    </>
   );
 };
 
